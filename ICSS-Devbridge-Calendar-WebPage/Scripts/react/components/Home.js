08255f3204674773cb3a375427ee78904dced4ef
entorius <eligijus.puplinskas@gmail.com>
2020-04-09 19:01:37 +0300

    -Added home page -Added sideBar (side component)

--- ICSS-Devbridge-Calendar-WebPage/Scripts/react/components/Home.js
+++ ICSS-Devbridge-Calendar-WebPage/Scripts/react/components/Home.js
@@ -1,18 +1,203 @@
 ﻿import React, { Component } from 'react';
 import SideBar from "./SideBar";
+import { withStyles } from '@material-ui/core/styles';
+//Material UI components
+import Button from '@material-ui/core/Button';
+
+import { Link } from "react-router-dom";
+
+//Icons
+
+import ForwardIcon from '@material-ui/icons/Forward';
+
+const styles = theme => ({
+    mainPage: {
+        display: 'flex',
+        height: '1100px'
+    },
+    homePageStyle: {
+        width:'80%'
+    },
+    homeCalendarComponent: {
+        width: '100%',
+        height: '550px',
+        backgroundColor: '#5A5A5A',
+        display: 'flex'
+    },
+    homeLearningTreeComponent: {
+        width: '100%',
+        height: '550px',
+        backgroundColor: '#5A5A5A',
+        display: 'flex'
+    },
+    imageSideCalendar: {
+        height: '100%',
+        width: '60%',
+        backgroundColor: '#B2EBF2',
+        display: 'flex'
+    },
+    imageSideLearningTree: {
+        height: '100%',
+        width: '60%',
+        backgroundColor: '#9BD5CC',
+        display: 'flex'
+    },
+   
+    triangleRight: {
+        width: 0,
+        height: 0,
+        borderTop: '550px solid #5A5A5A',
+        borderRight: '150px solid transparent',
+        zIndex: '1'
+    },
+    triangleLeft: {
+        width: '0',
+        height: '0',
+        marginLeft:'auto',
+        borderBottom: '550px solid #5A5A5A',
+        borderLeft: '150px solid transparent',
+        zIndex: '1'
+    },
+    calendarImage: {
+        backgroundImage: "url('../../../Assets/Calendar.jpg')",
+        backgroundPosition: 'center',
+        backgroundSize: '625px 400px',
+        height: '400px',
+        width: '625px',
+        backgroundRepeat: 'no-repeat',
+        marginTop: '70px',
+        marginLeft: '-60px'
+    },
+    treeImage: {
+        backgroundImage: "url('../../../Assets/tree.gif')",
+        backgroundPosition: 'center',
+        backgroundSize: '600px 450px',
+        height: '450px',
+        width: '600px',
+        backgroundRepeat: 'no-repeat'
+    },
+    cover: {
+        width: '40%',
+        height: '100%',
+        backgroundColor: '#5A5A5A',
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    componentCalendarText: {
+        fontSize: '30px',
+        fontWeight: 'bold',
+        color: '#42A5F6',
+        textAlign: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '80%'
+
+    },
+    componentLearningTreeText: {
+        fontSize: '30px',
+        fontWeight: 'bold',
+        color: '#00A9A3',
+        textAlign: 'center',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '80%'
+
+    },
+    link: {
+        display: 'flex',
+        height: '80px',
+    },
+    linkCalendarButton: {
+        border: 'solid #42A5F6',
+        backgroundColor: '#B2EBF2',
+        color: '#42A5F6',
+        marginLeft: '20px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '50%',
+        marginTop: '20px'
+
+    },
+    linkLearningTreeButton: {
+        border: 'solid #00A9A3',
+        backgroundColor: '#9BD5CC',
+        color: '#00A9A3',
+        marginLeft: '20px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '50%',
+        marginTop: '20px'
+    },
+    linkButtonPicture: {
+        marginRight: '10px'
+    },
+    linkButtonLabel: {
+        fontSize: '14px'
+    },
+});
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
+        const { classes } = this.props;
         return (
-            <div>
+            <div className={classes.mainPage} >
                 <SideBar />
-                <h1>This is a Home Page</h1>
+                <div className={classes.homePageStyle}>
+
+                    <div className={classes.homeCalendarComponent}>
+
+                        <div className={classes.cover} >
+                            <div className={classes.componentCalendarText}>
+                                Check and add 
+                                your learning days
+                                to calendar
+                            </div>
+                            <Link to="/Main/Calendar" className={classes.link}>
+                                <Button className={classes.linkCalendarButton} classes={{ label: classes.linkButtonLabel }}>
+                                GO TO CALENDAR
+                                <ForwardIcon className={classes.linkButtonPicture} />
+                                </Button>
+                            </Link>
+                        </div>
+                        <div className={classes.imageSideCalendar}>
+
+                            <div className={classes.triangleRight}/>
+                            <div className={classes.calendarImage}/>
+                            
+                        </div>
+
+                    </div>
+                    <div className={classes.homeLearningTreeComponent} >
+
+                        <div className={classes.imageSideLearningTree}>
+
+                            <div className={classes.treeImage} />
+                            <div className={classes.triangleLeft} />
+                        </div>
+
+                        <div className={classes.cover} >
+                            <div className={classes.componentLearningTreeText}>
+                                Grow your progress in learning tree
+                            </div>
+                            <Link to="/Main/LearningTree" className={classes.link}>
+                                <Button className={classes.linkLearningTreeButton} classes={{ label: classes.linkButtonLabel }}>
+                                GO TO LEARNING TREE
+                                <ForwardIcon className={classes.linkButtonPicture} />
+                                </Button>
+                            </Link>
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
         );
     }
 }
 
-export default Home;
+export default withStyles(styles)(Home);
--- ICSS-Devbridge-Calendar-WebPage/Scripts/react/components/SideBar.js
+++ ICSS-Devbridge-Calendar-WebPage/Scripts/react/components/SideBar.js
@@ -1,38 +1,145 @@
 ﻿import React, { Component } from 'react';
+//Styles providers
+import { withStyles } from '@material-ui/core/styles';
+
+//Material UI components
+import Button from '@material-ui/core/Button';
+
 import { Link } from "react-router-dom";
 
+//Icons
+
+import HomeIcon from '@material-ui/icons/Home';
+import GroupIcon from '@material-ui/icons/Group';
+import AssignmentIcon from '@material-ui/icons/Assignment'; //Topics
+import EventIcon from '@material-ui/icons/Event';
+import AccountTreeIcon from '@material-ui/icons/AccountTree';
+import SettingsIcon from '@material-ui/icons/Settings';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+const styles = theme => ({
+ 
+    sideBarDiv: {
+        width: '20%',
+        height: '100%',
+        display: 'flex',
+        flexWrap: 'wrap',
+        backgroundColor: '#555555',
+        flexDirection: 'column',
+        minWidth: '250px'
+    },
+    logo: {
+        backgroundImage: "url('../../../Assets/devbridgeLogo.png')",
+        backgroundSize: '70px 70px',
+        height: '70px',
+        width: '70px',
+        backgroundRepeat: 'no-repeat'
+    },
+    logoName: {
+        paddingTop: '20px',
+        paddingLeft: '20px',
+        fontSize: '20px',
+        fontWeight: 'bold',
+        color: '#ffffff'
+    },
+    logoGroup: {
+        display:'flex'
+    },
+    circle: {
+        width: '80px',
+        height: '80px',
+        background: '#D9001B',
+        borderRadius: '50%',
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        padding: '22px',
+        color: '#ffffff',
+        fontWeight: 'bold',
+        marginTop: '50px',
+        fontSize: '25px'
+    },
+    links: {
+    },
+    link: {
+        width: '100%',
+        display: 'flex',
+        marginTop: '30px',
+        borderBottom: 'solid 2px #696969'
+    },
+    linkButton: {
+        backgroundColor: '#555555',
+        border: 0,
+        color: '#ffffff',
+        marginLeft: '20px'
+    },
+    linkButtonLabel: {
+        fontSize: '14px'
+    },
+    linkButtonPicture: {
+        marginRight: '10px'
+    }
+
+});
 class SideBar extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-                <h1>This is a Sidebar Component</h1>
-                <Link to="/Main/Home">
-                    <button>HOME</button>
-                </Link>
-                <Link to="/Main/Team">
-                    <button>TEAM</button>
-                </Link>
-                <Link to="/Main/Topics">
-                    <button>TOPICS</button>
-                </Link>
-                <Link to="/Main/Calendar">
-                    <button>CALENDAR</button>
-                </Link>
-                <Link to="/Main/LearningTree">
-                    <button>LEARNING TREE</button>
-                </Link>
-                <Link to="/Main/Settings">
-                    <button>SETTINGS</button>
-                </Link>
-                <Link to="/">
-                    <button>LOGOUT</button>
-                </Link>
+            <div className={classes.sideBarDiv} >
+                <div className={classes.logoGroup}>
+                    <div className={classes.logo} />
+                    <div className={classes.logoName}>DEVBRIDGE</div>
+                </div>
+                <div className={classes.circle} > SG </div>
+                <div className={classes.links}>
+                    <Link to="/Main/Home" className={classes.link}>
+                        <Button className={classes.linkButton} classes={{ label: classes.linkButtonLabel }}>
+                            <HomeIcon className={classes.linkButtonPicture}/>
+                            HOME
+                            </Button>
+                    </Link>
+                    <Link to="/Main/Team" className={classes.link}>
+                        <Button className={classes.linkButton} classes={{ label: classes.linkButtonLabel }}>
+                            <GroupIcon className={classes.linkButtonPicture}/>
+                            TEAM
+                            </Button>
+                    </Link>
+                    <Link to="/Main/Topics" className={classes.link}>
+                        <Button className={classes.linkButton} classes={{ label: classes.linkButtonLabel }}>
+                            <AssignmentIcon className={classes.linkButtonPicture}/>
+                            TOPICS
+                            </Button>
+                    </Link>
+                    <Link to="/Main/Calendar" className={classes.link}>
+                        <Button className={classes.linkButton} classes={{ label: classes.linkButtonLabel }}>
+                            <EventIcon className={classes.linkButtonPicture}/>
+                            CALENDAR
+                            </Button>
+                    </Link>
+                    <Link to="/Main/LearningTree" className={classes.link}>
+                        <Button className={classes.linkButton} classes={{ label: classes.linkButtonLabel }}>
+                            <AccountTreeIcon className={classes.linkButtonPicture}/>
+                            LEARNING TREE
+                            </Button>
+                    </Link>
+                    <Link to="/Main/Settings" className={classes.link}>
+                        <Button className={classes.linkButton} classes={{ label: classes.linkButtonLabel }}>
+                            <SettingsIcon className={classes.linkButtonPicture}/>
+                            SETTINGS
+                            </Button>
+                    </Link>
+                    <Link to="/" className={classes.link}>
+                        <Button className={classes.linkButton} classes={{ label: classes.linkButtonLabel }}>
+                            <ExitToAppIcon className={classes.linkButtonPicture}/>
+                            LOGOUT
+                            </Button>
+                        </Link>
+                </div>
             </div>
         );
     }
 }
 
-export default SideBar;
+export default withStyles(styles)(SideBar);
