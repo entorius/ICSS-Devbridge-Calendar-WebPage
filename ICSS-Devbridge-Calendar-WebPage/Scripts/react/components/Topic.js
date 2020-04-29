26f147949203838aaf0a68630de9a67aa74740fc
dovydasjaru <dovydas0077@gmail.com>
2020-04-29 19:43:15 +0300

    Add Topic and Topics

---
+++ ICSS-Devbridge-Calendar-WebPage/Scripts/react/components/Topic.js
@@ -0,0 +1,17 @@
+﻿import React from "react";
+
+class Topic extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Hello</p>
+            </div>
+        );
+    }
+}
+
+export default Topic;
---
+++ ICSS-Devbridge-Calendar-WebPage/Scripts/react/containers/Topics.js
@@ -0,0 +1,90 @@
+﻿import React from 'react';
+import SideBar from "../components/SideBar";
+import Topic from "../components/Topic";
+import { withStyles } from '@material-ui/core/styles';
+
+//Redux
+import { connect } from 'react-redux';
+import { fetchAssignments } from '../redux/actions/assignmentActions';
+import PropTypes from 'prop-types';
+
+
+// Material UI components
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { Link } from "react-router-dom";
+import TreeView from '@material-ui/lab/TreeView';
+import TreeItem from '@material-ui/lab/TreeItem';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Input from '@material-ui/core/Input';
+
+//Icons
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import CreateIcon from '@material-ui/icons/Create';
+import AddIcon from '@material-ui/icons/Add';
+import CloseIcon from '@material-ui/icons/Close';
+import CompareArrowsIcon from '@material-ui/icons/CompareArrows';
+import PersonIcon from '@material-ui/icons/Person';
+
+const styles = theme => ({
+    root: {
+        minHeight: '100vh',
+        display: 'flex'
+    },
+    GridContainerStyle: {
+        direction: 'row',
+        justify: 'space-evenly',
+        alignItems: 'flex-start'
+    },
+    GridItemStyle: {
+        xs: '3'
+    }
+});
+
+class Topics extends React.Component {
+    state = {
+        topics: [
+            { id: 0, name: "item0" },
+            { id: 1, name: "item1" }
+        ]
+    };
+
+    AddTopics() {
+        var topics = this.state.topics;
+        for (var i = 1; i < 5; i++)
+            topics.push({ id: i, name: i + " item" });
+        this.setState({ topics });
+    };
+
+    componentDidMount() {
+        // ask for topics from server
+        //this.AddTopics(); 
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <SideBar />
+                <Grid container className={classes.GridContainerType}>
+                    {this.state.topics.map(topic =>
+                        <Grid container item xs="3">
+                            <Topic />
+                        </Grid>)}
+                </Grid>
+            </div>
+            );
+    }
+}
+
+export default withStyles(styles)(Topics);
