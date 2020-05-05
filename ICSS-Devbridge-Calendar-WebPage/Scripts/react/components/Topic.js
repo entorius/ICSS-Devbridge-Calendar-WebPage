bf1d899ec31d8f8d2d5e33005df77f8b622b18cd
dovydasjaru <dovydas0077@gmail.com>
2020-05-05 21:06:42 +0300

    Define topic component

--- ICSS-Devbridge-Calendar-WebPage/Scripts/react/components/Topic.js
+++ ICSS-Devbridge-Calendar-WebPage/Scripts/react/components/Topic.js
@@ -1,17 +1,46 @@
 ﻿import React from "react";
 
-class Topic extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+import Grid from '@material-ui/core/Grid';
 
+const topicNameStyle = {
+    textSize: 20,
+    display: "inline",
+    color: "black",
+    textAlign: "center",
+    margin: 0
+};
+const GridItemStyle = {
+    backgroundColor: "cyan",
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 5,
+    marginRight: 5
+};
+
+class Topic extends React.Component {
     render() {
         return (
-            <div>
-                <p>Hello</p>
-            </div>
+            <Grid container item xs="3" direction="column" style={GridItemStyle}>
+                <p style={topicNameStyle}>{this.props.name}</p>
+                <ColoredLine color="white" />
+                <p>Description: {this.props.name}</p>
+                <ColoredLine color="white" />
+            </Grid>
         );
     }
 }
+
+const ColoredLine = ({ color }) => (
+    <hr
+        style={{
+            color: color,
+            backgroundColor: color,
+            height: 1,
+            margin: 0
+        }}
+    />
+);
 
 export default Topic;
